32082a85b7e29670eb4c926cc4bee98bda174c91
c0defre4k <c0defre4k@users.noreply.github.com>
2019-01-22 16:28:43 +0100

    support for row prop in textarea

--- lib/templates/textarea.jsx
+++ lib/templates/textarea.jsx
@@ -1,12 +1,18 @@
 export default function(h) {
+const props = {
+    placeholder: this.placeholder,
+    disabled: this.disabled
+}
+if (this.rows) {
+    props.rows = this.rows
+}
 return <textarea
         name={this.name}
         id={"textarea_" + this.name}
         class="form-control"
         on-change={this.updateValue.bind(this)}
         on-keyup={this.updateValue.bind(this)}
-        disabled={this.disabled}
-        placeholder={this.placeholder}>
+        {...props}>
         	{this.curValue}
         </textarea>
 
