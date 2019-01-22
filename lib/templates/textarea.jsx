7dfecc4da7e879341ee8879d24bf6a713caab9da
c0defre4k <c0defre4k@users.noreply.github.com>
2019-01-22 17:01:42 +0100

    Revert "support for row prop in textarea"
    This reverts commit 32082a85b7e29670eb4c926cc4bee98bda174c91.

--- lib/templates/textarea.jsx
+++ lib/templates/textarea.jsx
@@ -1,18 +1,12 @@
 export default function(h) {
-const props = {
-    placeholder: this.placeholder,
-    disabled: this.disabled
-}
-if (this.rows) {
-    props.rows = this.rows
-}
 return <textarea
         name={this.name}
         id={"textarea_" + this.name}
         class="form-control"
         on-change={this.updateValue.bind(this)}
         on-keyup={this.updateValue.bind(this)}
-        {...props}>
+        disabled={this.disabled}
+        placeholder={this.placeholder}>
         	{this.curValue}
         </textarea>
 
